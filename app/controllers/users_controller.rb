c5f29b8623579e13de3054b6983c07f01baafcf4
Pooja Karale <pooja@peerlogicsys.com>
2020-10-24 11:37:27 +0000

    display users list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -11,6 +15,12 @@ class UsersController < ApplicationController
     else
       render 'new'
     end 
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+    puts @articles
   end
 
   def edit
